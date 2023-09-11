32f00c389f93339a3c21b0255fb7255a3654967e
Sarifa <sarifajusmar@gmail.com>
2023-09-11 09:55:49 +0700

    message commit

--- app/src/main/java/com/can/belajarbahasajepang/ui/home/HomeActivityN5.kt
+++ app/src/main/java/com/can/belajarbahasajepang/ui/home/HomeActivityN5.kt
@@ -9,15 +9,21 @@ import com.can.belajarbahasajepang.ui.kanji.KanjiActivity
 import com.can.belajarbahasajepang.ui.penjelasan.PenjelasanActivity
 import com.can.belajarbahasajepang.ui.subbunpo.SubbunpoActivity
 import com.can.belajarbahasajepang.ui.subkosa.SubkosaActivity
+import com.google.android.gms.ads.AdError
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
 class HomeActivityN5 : AppCompatActivity() {
     // binding
     private lateinit var binding: ActivityHomeN5Binding
-    // banner ads
-    private lateinit var mAdView : AdView
+    // interstitial
+    private var mInterstitialAd: InterstitialAd? = null
+    private var addCount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,68 +35,79 @@ class HomeActivityN5 : AppCompatActivity() {
             finish()
         }
 
-        // google banner ads
+        // interstitial ads
         MobileAds.initialize(this) {}
-
-        mAdView = binding.adView
-        val adRequest = AdRequest.Builder().build()
-        mAdView.loadAd(adRequest)
+        loadAds()
 
         // btn to SubkosaActivity
         binding.kosaCv.setOnClickListener {
-            val moveIntent = Intent(this@HomeActivityN5, SubkosaActivity::class.java)
-            val levelResponseItem = LevelResponseItem(
-                "N5 Level",
-                "2020-07-13 03:30:35",
-                "2020-07-13 03:30:35",
-                "5f0b72ebb3ed3",
-                "User123"
-            )
-            moveIntent.putExtra("levelResponseItem", levelResponseItem)
-            startActivity(moveIntent)
+            showAds(SubkosaActivity::class.java)
         }
 
         // btn to SubbunpoActivity
         binding.bunpoCv.setOnClickListener {
-            val moveIntent = Intent(this@HomeActivityN5, SubbunpoActivity::class.java)
-            val levelResponseItem = LevelResponseItem(
-                "N5 Level",
-                "2020-07-13 03:30:35",
-                "2020-07-13 03:30:35",
-                "5f0b72ebb3ed3",
-                "User123"
-            )
-            moveIntent.putExtra("levelResponseItem", levelResponseItem)
-            startActivity(moveIntent)
+            showAds(SubbunpoActivity::class.java)
         }
 
         // btn to KanjiActivity
         binding.kanjiCv.setOnClickListener {
-            val moveIntent = Intent(this@HomeActivityN5, KanjiActivity::class.java)
-            val levelResponseItem = LevelResponseItem(
-                "N5 Level",
-                "2020-07-13 03:30:35",
-                "2020-07-13 03:30:35",
-                "5f0b72ebb3ed3",
-                "User123"
-            )
-            moveIntent.putExtra("levelResponseItem", levelResponseItem)
-            startActivity(moveIntent)
+            showAds(KanjiActivity::class.java)
         }
 
         // btn to PenjelasanActivity
         binding.penjelasanCv.setOnClickListener {
-            val moveIntent = Intent(this@HomeActivityN5, PenjelasanActivity::class.java)
-            val levelResponseItem = LevelResponseItem(
-                "N5 Level",
-                "2020-07-13 03:30:35",
-                "2020-07-13 03:30:35",
-                "5f0b72ebb3ed3",
-                "User123"
-            )
-            moveIntent.putExtra("levelResponseItem", levelResponseItem)
-            startActivity(moveIntent)
+            showAds(PenjelasanActivity::class.java)
         }
+    }
+    private fun navigateToNextActivity(destinationActivity: Class<*>) {
+        addCount+=1
+        val moveIntent = Intent(this@HomeActivityN5, destinationActivity)
+        val levelResponseItem = LevelResponseItem(
+            "N5 Level",
+            "2020-07-13 03:30:35",
+            "2020-07-13 03:30:35",
+            "5f0b72ebb3ed3",
+            "User123"
+        )
+        moveIntent.putExtra("levelResponseItem", levelResponseItem)
+        startActivity(moveIntent)
+    }
 
+    private fun loadAds() {
+        val adRequest = AdRequest.Builder().build()
+
+        InterstitialAd.load(this, "ca-app-pub-1053226146342475/7594320573", adRequest, object : InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                mInterstitialAd = null
+            }
+
+            override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                mInterstitialAd = interstitialAd
+            }
+        })
+    }
+
+    private fun showAds(destinationActivity: Class<*>) {
+        if (mInterstitialAd != null) {
+            if (addCount % 5 == 0) {
+                mInterstitialAd?.show(this)
+                mInterstitialAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
+                    override fun onAdDismissedFullScreenContent() {
+                        mInterstitialAd = null
+                        navigateToNextActivity(destinationActivity)
+                    }
+
+                    override fun onAdFailedToShowFullScreenContent(p0: AdError) {
+                        mInterstitialAd = null
+                        navigateToNextActivity(destinationActivity)
+                    }
+                }
+            }
+            else {
+                navigateToNextActivity(destinationActivity)
+            }
+        } else {
+            navigateToNextActivity(destinationActivity)
+        }
     }
 }
